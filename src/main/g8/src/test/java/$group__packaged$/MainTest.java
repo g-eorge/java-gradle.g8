5b0915035fb39d6d116c18f007adc1fadb021ec7
George Agnelli <george.agnelli@gmail.com>
2014-02-23 17:57:33 +0000

    change structure

---
+++ src/main/g8/src/test/java/$group__packaged$/MainTest.java
@@ -0,0 +1,22 @@
+package $group$;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class MainTest {
+  
+  @Test
+  public void thisAlwaysPasses() {
+  }
+
+  @Test
+  @Ignore
+  public void thisIsIgnored() {
+  }
+
+}
